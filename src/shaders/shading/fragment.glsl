48f022bd1430f48141264cf13524cb7e6c0e0861
soloiaros <soloiaros@gmail.com>
2026-09-13 13:04:24 +0300

    feat: implement directional light & specular highlights

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,20 +1,29 @@
 uniform vec3 uColor;
 
 varying vec3 vNormal;
+varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 
-vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition) {
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection, float specularPower) {
     vec3 lightDirection = normalize(lightPosition);
+    vec3 lightReflection = reflect(- lightDirection, normal);
 
     // Shading
     float shading = max(0.0, dot(normal, lightDirection));
+
+    // Specular
+    float specular = max(0.0, - dot(lightReflection, viewDirection));
+    specular = pow(specular, specularPower);
     
-    return lightColor * lightIntensity * shading;
+    return lightColor * lightIntensity * (shading + specular);
 }
 
 void main() {
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 color = uColor;
+
+    vec3 normal = normalize(vNormal);
 
     /**
     * Light
@@ -26,9 +35,11 @@ void main() {
     );
     light += directionalLight(
         vec3(0.1, 0.1, 1.0),
-        0.5,
-        vNormal,
-        vec3(0.0, 0.0, 3.0)
+        1.0,
+        normal,
+        vec3(0.0, 0.0, 3.0),
+        viewDirection,
+        20.0
     );
 
     color *= light;
