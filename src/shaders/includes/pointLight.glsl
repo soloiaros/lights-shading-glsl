630422613383c193d2de181ce041d7231dc3d237
soloiaros <soloiaros@gmail.com>
2026-09-13 13:20:07 +0300

    feat: implement decay for point light

--- src/shaders/includes/pointLight.glsl
+++ src/shaders/includes/pointLight.glsl
@@ -1,5 +1,6 @@
-vec3 pointLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection, float specularPower, vec3 position) {
+vec3 pointLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection, float specularPower, vec3 position, float lightDecay) {
     vec3 lightDelta = lightPosition - position;
+    float lightDistance = length(lightDelta);
     vec3 lightDirection = normalize(lightDelta);
     vec3 lightReflection = reflect(- lightDirection, normal);
 
@@ -9,6 +10,10 @@ vec3 pointLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPo
     // Specular
     float specular = max(0.0, - dot(lightReflection, viewDirection));
     specular = pow(specular, specularPower);
+
+    // Decay
+    float decay = 1.0 - lightDistance * lightDecay;
+    decay = max(0.0, decay);
     
-    return lightColor * lightIntensity * (shading + specular);
+    return lightColor * lightIntensity * (shading + specular) * decay;
 }
--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -17,18 +17,18 @@ void main() {
     * Light
     **/
     vec3 light = vec3(0.0);
-    // light += ambientLight(
-    //     vec3(1.0),
-    //     0.03
-    // );
-    // light += directionalLight(
-    //     vec3(0.1, 0.1, 1.0),
-    //     1.0,
-    //     normal,
-    //     vec3(0.0, 0.0, 3.0),
-    //     viewDirection,
-    //     20.0
-    // );
+    light += ambientLight(
+        vec3(1.0),
+        0.03
+    );
+    light += directionalLight(
+        vec3(0.1, 0.1, 1.0),
+        1.0,
+        normal,
+        vec3(0.0, 0.0, 3.0),
+        viewDirection,
+        20.0
+    );
     light += pointLight(
         vec3(1.0, 0.1, 0.1),
         1.0,
@@ -36,7 +36,8 @@ void main() {
         vec3(0.0, 2.5, 0.0),
         viewDirection,
         20.0,
-        vPosition
+        vPosition,
+        0.25
     );
 
     color *= light;
