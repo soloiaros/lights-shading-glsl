040c0400ed5c3349cb6adfb692a20fbd8398857b
soloiaros <soloiaros@gmail.com>
2026-09-13 12:02:29 +0300

    feat: add shader ambient light

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,8 +1,22 @@
 uniform vec3 uColor;
 
-void main()
-{
+vec3 ambientLight(vec3 lightColor, float lightIntensity) {
+    return lightColor * lightIntensity;
+}
+
+void main() {
     vec3 color = uColor;
+
+    /**
+    * Light
+    **/
+    vec3 light = vec3(0.0);
+    light += ambientLight(
+        vec3(1.0), // Light color
+        0.03 // Light intensity
+    );
+
+    color *= light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
--- src/shaders/shading/vertex.glsl
+++ src/shaders/shading/vertex.glsl
@@ -1,5 +1,4 @@
-void main()
-{
+void main() {
     // Position
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
